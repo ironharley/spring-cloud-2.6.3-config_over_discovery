a2566d87bccabb61e82ef66c33879466b17eb8bf
Олег Мицай <oamitsay@sberbank.ru>
2022-06-14 12:20:28 +0300

    not refreshed

--- demo/cloud-config/src/main/java/com/example/cloudconfig/CloudConfigApplication.java
+++ demo/cloud-config/src/main/java/com/example/cloudconfig/CloudConfigApplication.java
@@ -2,11 +2,13 @@ package com.example.cloudconfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableConfigurationProperties
 @EnableDiscoveryClient
 public class CloudConfigApplication {
 
---
+++ demo/test-rest/src/main/java/com/example/testrest/TestController.java
@@ -0,0 +1,22 @@
+package com.example.testrest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+@RefreshScope
+public class TestController {
+
+    @Value(("${app.test.value:empty}"))
+    private String appTestValue;
+
+    @GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public String get() {
+        return appTestValue;
+    }
+}
